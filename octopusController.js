56b110c6fcb5063162dfbc0ce36a6ecb330bdf83
Navid Matin <nsmnavid@gmail.com>
2016-03-29 17:44:10 -0400

    Added data request call

--- octopusController.js
+++ octopusController.js
@@ -1,93 +1,107 @@
+function octopusController(octopusServerInfo) {
 
-function octopusController(octopusServerInfo){
-    
-    
-	function createUrlWithKeyHeaderForOctopus(sectionUrl, callback)
-	{
-		octopusServerInfo.getOctopusServerInfo(function(result){
-			if(result)
-			{
+
+	function createUrlWithKeyHeaderForOctopus(sectionUrl, callback) {
+		octopusServerInfo.getOctopusServerInfo(function (result) {
+			if (result) {
 				var resultObject = JSON.parse(result);
 				callback({
-					address:"http://" + resultObject.address + sectionUrl,
-					parameter:resultObject.api
+					address: "http://" + resultObject.address + sectionUrl,
+					parameter: resultObject.api
 				});
 			}
 		})
 	}
 
 	//function for doing POST on Octopus REST-API
-	function octopusServerHttpPostRequest(url, apiKey, callback)
-	{
+	function octopusServerHttpPostRequest(url, apiKey, callback) {
 		var xhr = new XMLHttpRequest();
 		xhr.open("POST", url, true);
 
-		xhr.onreadystatechange = function() {//Call a function when the state changes.
-		    if(xhr.readyState == 4 && xhr.status == 200) {
-		        console.log(xhr.response);
-		        callback(xhr.response);
-		    }
+		xhr.onreadystatechange = function () { //Call a function when the state changes.
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				console.log(xhr.response);
+				callback(xhr.response);
+			}
 		}
 		xhr.setRequestHeader("X-Octopus-ApiKey", apiKey);
 		xhr.send(apiKey);
 	}
-	
+
 	//function for doing GET on Octopus REST-API (Parameters: [{name:"", value:""}])
-	function octopusServerHttpGetRequest(url, apiKey, callback, parameters)
-	{
+	function octopusServerHttpGetRequest(url, apiKey, callback, parameters) {
 		var xhr = new XMLHttpRequest();
-        var allOfParams = [];
+		var allOfParams = [];
 		xhr.open("GET", url, true);
 
-		xhr.onreadystatechange = function() {//Call a function when the state changes.
-		    if(xhr.readyState == 4 && xhr.status == 200) {
-		        console.log(xhr.response);
-		        callback(xhr.response);
-		    }
+		xhr.onreadystatechange = function () { //Call a function when the state changes.
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				console.log(xhr.response);
+				callback(xhr.response);
+			}
 		}
 		xhr.setRequestHeader("X-Octopus-ApiKey", apiKey);
-        allOfParams.push(apiKey);
-        if(parameters)
-        {
-            for(var i=0;i<parameters.length;i++)
-            {
-                xhr.setRequestHeader(parameters[i].name, parameters[i].value);
-                allOfParams.push(parameters[i].value);
-            }
-        }
+		allOfParams.push(apiKey);
+		if (parameters) {
+			for (var i = 0; i < parameters.length; i++) {
+				xhr.setRequestHeader(parameters[i].name, parameters[i].value);
+				allOfParams.push(parameters[i].value);
+			}
+		}
 
 		xhr.send(allOfParams);
 	}
 
-    //Gets all of the library variable sets_ callback = function(listOfLibraryVariableSets)
-    var getAllLibraryVariableSets = function(callback){
-        createUrlWithKeyHeaderForOctopus("/api/libraryvariablesets?contentType=Variables", function(result){
-			octopusServerHttpGetRequest(result.address, result.parameter, function(result){
-                if(result)
-                {
-                    var returnedObject = JSON.parse(result);
-                    var listOfLibraryVariableSets = [];
-                    for(var i=0;i<returnedObject.Items.length;i++)
-                    {
-                        listOfLibraryVariableSets.push(returnedObject.Items[i]);
-                    }
-                    callback(listOfLibraryVariableSets);
-                }
-            });
+	//Gets variables for a library variable set(it needs variable set id)
+	function getVariables(id, callback) {
+		createUrlWithKeyHeaderForOctopus("/api/variables/" + id, function (result) {
+			octopusServerHttpGetRequest(result.address, result.parameter, function (variableSet) {
+				var varSet = JSON.parse(variableSet);
+				callback(varSet);
+			});
+		});
+	}
+
+	//Gets all of the library variable sets_ callback = function(listOfLibraryVariableSets)
+	var getAllLibraryVariableSets = function (callback) {
+		createUrlWithKeyHeaderForOctopus("/api/libraryvariablesets?contentType=Variables", function (result) {
+			octopusServerHttpGetRequest(result.address, result.parameter, function (result) {
+				if (result) {
+					var returnedObject = JSON.parse(result);
+					var listOfLibraryVariableSets = [];
+					for (var i = 0; i < returnedObject.Items.length; i++) {
+						listOfLibraryVariableSets.push(returnedObject.Items[i]);
+					}
+					callback(listOfLibraryVariableSets);
+				}
+			});
 		})
-    }
-	var callAPI = function()
-	{
-		createUrlWithKeyHeaderForOctopus("/api/libraryvariablesets", function(result){
-			octopusServerHttpGetRequest(result.address, result.parameter, function(){}, [{name:"contentType", value:"Variables"}]);
-		})
+	}
+	//Gets the library variable set object with its variables from octopus [callback: function(result)]
+	var getLibraryVariableSetContent = function (id, callback) {
+		createUrlWithKeyHeaderForOctopus("/api/libraryvariablesets/" + id, function (result) {
+			//First get library variable set itself
+			octopusServerHttpGetRequest(result.address, result.parameter, function (libraryVariableSet) {
+				if (libraryVariableSet) {
+					var libVarSet = JSON.parse(libraryVariableSet);
+					//Now get the variables for this library variable set
+					getVariables(libVarSet.VariableSetId, function (varSet) {
+						if (varSet) {
+							callback({
+								LibraryVariableSet: libVarSet,
+								Variables: varSet
+							});
+						}
+					});
+				}
+			});
+		});
 
 	}
 
 
 	return {
-		callAPI: callAPI,
-        getAllLibraryVariableSets: getAllLibraryVariableSets
+		getAllLibraryVariableSets: getAllLibraryVariableSets,
+		getLibraryVariableSetContent: getLibraryVariableSetContent
 	}
 }
-
--- popup.js
+++ popup.js
@@ -8,202 +8,146 @@
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
+//Opens up a new pop up rather than attached:
+chrome.browserAction.onClicked.addListener(function() {
+   chrome.windows.create({'url': 'popup.html', 'type': 'popup'}, function(window) {
+   });
+});
 
- var octopusInfo = null;
- var octopusControllerInstance = null;
+var octopusInfo = null;
+var octopusControllerInstance = null;
+
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+	// Query filter to be passed to chrome.tabs.query - see
+	// https://developer.chrome.com/extensions/tabs#method-query
+	var queryInfo = {
+		active: true,
+		currentWindow: true
+	};
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+	chrome.tabs.query(queryInfo, function (tabs) {
+		// chrome.tabs.query invokes the callback with a list of tabs that match the
+		// query. When the popup is opened, there is certainly a window and at least
+		// one tab, so we can safely assume that |tabs| is a non-empty array.
+		// A window can only have one active tab at a time, so the array consists of
+		// exactly one tab.
+		var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+		// A tab is a plain object that provides information about the tab.
+		// See https://developer.chrome.com/extensions/tabs#type-Tab
+		var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+		// tab.url is only available if the "activeTab" permission is declared.
+		// If you want to see the URL of other tabs (e.g. after removing active:true
+		// from |queryInfo|), then the "tabs" permission is required to see their
+		// "url" properties.
+		console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-  });
+		callback(url);
+	});
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+	// Most methods of the Chrome extension APIs are asynchronous. This means that
+	// you CANNOT do something like this:
+	//
+	// var url;
+	// chrome.tabs.query(queryInfo, function(tabs) {
+	//   url = tabs[0].url;
+	// });
+	// alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-function isThereSavedInfo(pass){
-    //Instantiation octopusInfo with password
-    octopusInfo = octopusServerInfo(pass);
-    octopusInfo.doesSaveExist(function(result){
-    console.log(result);
-    if(!result)
-    {
-      showGetServerInfo();
-    }
-    else
-    {
-      showInfo();
-    }
-  });
+function isThereSavedInfo(pass) {
+	//Instantiation octopusInfo with password
+	octopusInfo = octopusServerInfo(pass);
+	octopusInfo.doesSaveExist(function (result) {
+		console.log(result);
+		if (!result) {
+			showGetServerInfo();
+		} else {
+			showInfo();
+		}
+	});
 
 }
 
-function saveServerInfo(){
+function saveServerInfo() {
 
-    var server = document.getElementById("server").value;
+	var server = document.getElementById("server").value;
 
-    octopusInfo.saveOctopusServerServerAddressAPIKey(server, document.getElementById("apiKey").value);
-    showInfo();
+	octopusInfo.saveOctopusServerServerAddressAPIKey(server, document.getElementById("apiKey").value);
+	showInfo();
 
 }
 
-function setupController()
+function setupController() {
+	octopusControllerInstance = octopusController(octopusInfo);
+}
+
+function setUpElements() {
+	document.getElementById("save").addEventListener("click", saveServerInfo);
+	document.getElementById('passkey').addEventListener('keyup', enterPass);
+	document.getElementById('password').style.display = "block";
+	document.getElementById("get").addEventListener("click", getLibraryVariableSetInfo);
+}
+
+function showGetServerInfo() {
+	document.getElementById('password').style.display = "none";
+	document.getElementById("getOctopusServer").display = "block";
+	document.getElementById("variableCopySection").style.display = "none";
+	document.getElementById("variableSetName").style.display = "none";
+}
+
+function showInfo() {
+	setupController();
+	octopusInfo.getOctopusServerInfo(function (result) {
+		if (result) {
+			document.getElementById('password').style.display = "none";
+			document.getElementById("getOctopusServer").style.display = "none";
+			document.getElementById("variableSetName").style.display = "block";
+			document.getElementById("variableCopySection").style.display = "none";
+			document.getElementById("badPassword").style.display = "none";
+			createVariableDropDown();
+			console.log(result);
+			document.getElementById("info").innerHTML = result;
+		} else {
+			document.getElementById("badPassword").style.display = "block";
+			result = "BAD PASSWORD";
+			console.log(result);
+		}
+	});
+}
+
+function createVariableDropDown() {
+	var selectStatement = document.getElementById("selectVar");
+	octopusControllerInstance.getAllLibraryVariableSets(function (result) {
+		for (var i = 0; i < result.length; i++) {
+			var option = document.createElement("option");
+			option.text = result[i].Name;
+			option.value = result[i].Id;
+			selectStatement.add(option);
+		}
+	});
+}
+
+function getLibraryVariableSetInfo()
 {
-    octopusControllerInstance = octopusController(octopusInfo);
-}
-function setUpElements(){
-    document.getElementById("save").addEventListener("click", saveServerInfo);
-    document.getElementById('passkey').addEventListener('keyup', enterPass);
-    document.getElementById('password').style.display = "block";
-}
-
-function showGetServerInfo(){
-  document.getElementById('password').style.display = "none";
-  document.getElementById("getOctopusServer").display = "block";
-  document.getElementById("variableCopySection").style.display = "none";
-  document.getElementById("variableSetName").style.display = "none";
+	//Get currently selected lib var set id
+	octopusControllerInstance.getLibraryVariableSetContent(document.getElementById("selectVar").value, function(result){
+		if(result)
+			{
+				document.getElementById("infoPanel").style.display = "block";
+				document.getElementById("panelTitle").innerHTML = result.LibraryVariableSet.Name;
+				document.getElementById("panelBody").innerHTML = JSON.stringify(result.Variables, null, 2);
+			}
+	});
 }
 
-function showInfo(){
-    setupController();
-    octopusInfo.getOctopusServerInfo(function(result){
-      if(result)
-      {
-        document.getElementById('password').style.display = "none";
-        document.getElementById("getOctopusServer").style.display = "none";
-        document.getElementById("variableSetName").style.display = "block";
-        document.getElementById("variableCopySection").style.display = "none";
-        document.getElementById("badPassword").style.display = "none";
-        createVariableDropDown();
-        console.log(result);
-        document.getElementById("info").innerHTML = result;
-      }
-      else
-      {
-        document.getElementById("badPassword").style.visibility = "visible";
-        result = "BAD PASSWORD";
-        console.log(result);
-      }
-    });
+function enterPass(e) {
+	if (e.keyCode == 13 && document.activeElement.id === "passkey") {
+		var pass = document.getElementById("passkey").value;
+		isThereSavedInfo(pass);
+	}
 }
-
-function createVariableDropDown(){
-    var selectStatement = document.getElementById("selectVar");
-    octopusControllerInstance.getAllLibraryVariableSets(function(result){
-        for(var i=0; i<result.length; i++)
-        {
-            var option = document.createElement("option");
-            option.text = result[i].Name;
-            option.value = result[i].Id;
-            selectStatement.add(option);
-        }
-    });
+window.onload = function () {
+	setUpElements();
 }
-function enterPass(e){
-      if (e.keyCode == 13 && document.activeElement.id === "passkey") {
-        var pass = document.getElementById("passkey").value;
-        isThereSavedInfo(pass);
-    }
-}
-window.onload = function (){
-  setUpElements();
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-// function getImageUrl(searchTerm, callback, errorCallback) {
-//   // Google image search - 100 searches per day.
-//   // https://developers.google.com/image-search/
-//   var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-//     '?v=1.0&q=' + encodeURIComponent(searchTerm);
-//   var x = new XMLHttpRequest();
-//   x.open('GET', searchUrl);
-//   // The Google image search API responds with JSON, so let Chrome parse it.
-//   x.responseType = 'json';
-//   x.onload = function() {
-//     // Parse and process the response from Google Image Search.
-//     var response = x.response;
-//     if (!response || !response.responseData || !response.responseData.results ||
-//         response.responseData.results.length === 0) {
-//       errorCallback('No response from Google Image search!');
-//       return;
-//     }
-//     var firstResult = response.responseData.results[0];
-//     // Take the thumbnail instead of the full image to get an approximately
-//     // consistent image size.
-//     var imageUrl = firstResult.tbUrl;
-//     var width = parseInt(firstResult.tbWidth);
-//     var height = parseInt(firstResult.tbHeight);
-//     console.assert(
-//         typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-//         'Unexpected respose from the Google Image Search API!');
-//     callback(imageUrl, width, height);
-//   };
-//   x.onerror = function() {
-//     errorCallback('Network error.');
-//   };
-//   x.send();
-// }
-
-// function renderStatus(statusText) {
-//   document.getElementById('status').textContent = statusText;
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   getCurrentTabUrl(function(url) {
-//     // Put the image URL in Google search.
-//     renderStatus('Performing Google Image search for ' + url);
-
-//     getImageUrl(url, function(imageUrl, width, height) {
-
-//       renderStatus('Search term: ' + url + '\n' +
-//           'Google image search result: ' + imageUrl);
-//       var imageResult = document.getElementById('image-result');
-//       // Explicitly set the width/height to minimize the number of reflows. For
-//       // a single image, this does not matter, but if you're going to embed
-//       // multiple external images in your page, then the absence of width/height
-//       // attributes causes the popup to resize multiple times.
-//       imageResult.width = width;
-//       imageResult.height = height;
-//       imageResult.src = imageUrl;
-//       imageResult.hidden = false;
-
-//     }, function(errorMessage) {
-//       renderStatus('Cannot display image. ' + errorMessage);
-//     });
-//   });
-// });
